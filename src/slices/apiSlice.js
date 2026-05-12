8a9227a9c0a0770cc410c4b64b670cb6457c617e
Satyam kumar <satyamkumar2716@gmail.com>
2026-05-13 03:46:09 +0530

    update

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,14 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../constants';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers) => {
+    // any extra headers
+    return headers;
+  },
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
   baseQuery,
