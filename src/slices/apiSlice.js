dcf36cf296c09283e409650642661837f6328408
Satyam kumar <satyamkumar2716@gmail.com>
2026-05-13 04:01:23 +0530

    update

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,17 +1,24 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
+import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({ 
   baseUrl: BASE_URL,
-  prepareHeaders: (headers) => {
-    // any extra headers
-    return headers;
-  },
   credentials: 'include',
 });
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    api.dispatch(logout());
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Product', 'Order', 'User', 'Category'],
   endpoints: (builder) => ({}),
 });
